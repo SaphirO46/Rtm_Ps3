a4fe659936883ca9154236126b132118a25b9041
Saphiro46 <lele66.w@gmail.com>
2023-07-05 15:07:03 +0200

    modif fonts et boutton info

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBarKnockback = new System.Windows.Forms.TrackBar();
+            this.trackBarFov = new System.Windows.Forms.TrackBar();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarKnockback)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarFov)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 37);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 54);
+            this.button1.Size = new System.Drawing.Size(220, 48);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Connexion";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(241, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Attach";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(206)))), ((int)(((byte)(234)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(226)))), ((int)(((byte)(234)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(135, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 78);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "KNOCKBACK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(206)))), ((int)(((byte)(234)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(226)))), ((int)(((byte)(234)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(135, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 78);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "GODMODE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(206)))), ((int)(((byte)(234)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(226)))), ((int)(((byte)(234)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(135, 382);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(193, 78);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "FOV";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBarKnockback
+            // 
+            this.trackBarKnockback.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.trackBarKnockback.Location = new System.Drawing.Point(334, 288);
+            this.trackBarKnockback.Minimum = 1;
+            this.trackBarKnockback.Name = "trackBarKnockback";
+            this.trackBarKnockback.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarKnockback.Size = new System.Drawing.Size(56, 78);
+            this.trackBarKnockback.TabIndex = 6;
+            this.trackBarKnockback.Value = 1;
+            this.trackBarKnockback.Scroll += new System.EventHandler(this.trackBarKnockback_Scroll);
+            // 
+            // trackBarFov
+            // 
+            this.trackBarFov.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.trackBarFov.Location = new System.Drawing.Point(334, 382);
+            this.trackBarFov.Minimum = 1;
+            this.trackBarFov.Name = "trackBarFov";
+            this.trackBarFov.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarFov.Size = new System.Drawing.Size(56, 78);
+            this.trackBarFov.TabIndex = 7;
+            this.trackBarFov.Value = 1;
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonInfo.FlatAppearance.BorderSize = 0;
+            this.buttonInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInfo.Font = new System.Drawing.Font("Oxanium Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonInfo.Location = new System.Drawing.Point(12, 593);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(55, 48);
+            this.buttonInfo.TabIndex = 8;
+            this.buttonInfo.Text = "?";
+            this.buttonInfo.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(665, 486);
+            this.ClientSize = new System.Drawing.Size(482, 653);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.trackBarFov);
+            this.Controls.Add(this.trackBarKnockback);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(5000, 7000);
+            this.MinimumSize = new System.Drawing.Size(500, 700);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SaphirO\'s RTM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarKnockback)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarFov)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBarKnockback;
+        private System.Windows.Forms.TrackBar trackBarFov;
+        private System.Windows.Forms.Button buttonInfo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace Rtm_Ps3
 
     public partial class Form1 : Form
     {
+        bool button4Activation = false;
         private PS3API PS3 = new PS3API();
         private Random rand = new Random();
         public Form1()
@@ -27,24 +28,344 @@ namespace Rtm_Ps3
             PS3.ChangeAPI(SelectAPI.ControlConsole);
             if (PS3.ConnectTarget())
             {
-                string Message = "tu est conectée avec le SimplyLife by Paranctiss v1  ";
-                MessageBox.Show(Message, "Connecté !", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                string Message = "tu est conectée";
+                MessageBox.Show(Message, "Connected !", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
             else
             {
                 string Message = "Connexion échouée :(";
-                MessageBox.Show(Message, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error); //connexion
+                MessageBox.Show(Message, "ERROR !", MessageBoxButtons.OK, MessageBoxIcon.Error); //connexion
             }
 
 
 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             if (PS3.AttachProcess())
             {
-                MessageBox.Show("Attaché avec succé !.", "Parfait !", MessageBoxButtons.OK, MessageBoxIcon.Question);
-                PS3.CCAPI.Notify(CCAPI.NotifyIcon.FRIEND, "PS3 Prête !");
+                
+                PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "CONNECTED");
             }
-            else { MessageBox.Show("Probleme lors de l'attachement", "Erreur...", MessageBoxButtons.OK, MessageBoxIcon.Error); } //Attach
+            else { MessageBox.Show("ERROR", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error); } //Attach
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            switch (trackBarKnockback.Value)
+            {
+                case 1:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x01 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x10 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x20 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 4:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x30 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 5:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x40 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 6:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x60 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 7:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0x90 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 8:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0xA5 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 9:
+                    try
+                    {
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0xC5 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 10:
+
+                    try
+                    {
+
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0xFF });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+
+
+                default:
+                    try
+                    {
+                        MessageBox.Show("you need a select value ");
+
+                        PS3.SetMemory(0x003A4018, new byte[] { 0x40, 0xE5 });
+                        string MessageKn = Convert.ToString(trackBarKnockback.Value);
+                        PS3.CCAPI.Notify(CCAPI.NotifyIcon.PEN, "Knockback level:" + MessageKn);
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        private void trackBarKnockback_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!button4Activation) {
+                try {
+
+                    PS3.SetMemory(0x004B2021, new byte[] { 0x80 });
+
+                }
+                catch { MessageBox.Show("ERROR"); }
+            }
+            else if (button4Activation)
+            {
+                try
+                {
+
+                    PS3.SetMemory(0x004B2021, new byte[] { 0x20 });
+
+                }
+                catch { MessageBox.Show("ERROR"); }
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch (trackBarFov.Value)
+            {
+                case 1:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0xC5 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0xA5 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x95 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 4:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x80 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 5:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x70 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 6:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x60 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 7:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x45 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 8:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x30 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 9:
+                    try
+                    {
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x20 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+                case 10:
+
+                    try
+                    {
+
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x10 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+
+
+                default:
+                    try
+                    {
+                        MessageBox.Show("you need a select value ");
+
+                        PS3.SetMemory(0x014C670C, new byte[] { 0x3F, 0x80 });
+                    }
+                    catch (Exception erreur)
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                    break;
+            }
         }
     }
     }
